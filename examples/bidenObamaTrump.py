8a27284b3c9f472cc095649c0a14730a98b9a764
unknown <alch3mist94@protonmail.com>
2023-12-12 15:46:23 +0100

    pinned setuptools version

--- examples/bidenObamaTrump.py
+++ examples/bidenObamaTrump.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Generates a realistic conversation between Biden, Obama and Trump.
+Generates a realistic group chat between Biden, Obama and Trump.
 The presidents joke around and roast each other.
 '''
 from blowtorch import client
@@ -9,7 +9,7 @@ from blowtorch import client
 if __name__ == '__main__':
     print(client('llama-2-7b-chat.Q2_K.gguf', 'TheBloke/Llama-2-7B-Chat-GGUF', 
         device='cpu', model_type="llama").inference(
-                "Generate a trialog between Biden, Trump and Obama. The presidents should joke around related topics, roast and insult each other in a subtle way with real references, and the format should obey the scheme Joe: ... Donald: ... Obama: ..., no prologue just start directly with the conversation.",
+                "Generate a trialog between Biden, Trump and Obama in a group chat. The presidents should joke around related topics, roast and insult each other in a subtle way with real references, and the format should obey the scheme Joe: ... Donald: ... Obama: ..., no prologue just start directly with the conversation.",
                 512, 
                 do_sample=True, 
                 temperature=0.8, 
