df7d384082c105b57309ff29162cbbdd41013ce9
unknown <alch3mist94@protonmail.com>
2023-12-13 22:42:42 +0100

    bound submit method to UI

--- blowtorch/static/script.js
+++ blowtorch/static/script.js
@@ -4,6 +4,7 @@ createApp({
     data () {
         return {
             message: 'Hello Vue!',
+            context: {},
             count: 0
         }
     },
@@ -25,16 +26,21 @@ createApp({
             
             // decide which side
             if (side == 'l') {
-                messageBox.classList.add('left')
+                boxWrapper.classList.add('left')
             } else if (side == 'r') {
-                messageBox.classList.add('right')
+                boxWrapper.classList.add('right')
             }
 
             // add content
             messageBox.innerHTML = msg;
             
             boxWrapper.appendChild(messageBox);
-            chatWindow.appendChild(boxWrapper)
+            chatWindow.appendChild(boxWrapper);
+
+            // scroll to bottom
+            chatWindow.scrollTop = chatWindow.scrollHeight;
+
+            return messageBox
             
         },
         request (options, path, json=true) {
@@ -58,14 +64,29 @@ createApp({
                 xhr.send(JSON.stringify(options)); 
             });
         },
+        submit () {
+
+            const inputField = document.getElementById('msg-input-field');
+            const payload = inputField.value;
+            inputField.value = '';
+            
+            // build message box for chat window
+            this.messageBox(payload, 'r')
+        }
 
     },
     mounted () {
         try {
             // this.request({'data':[1,2,3]}, '/');
             console.log('mounted.');
-            this.messageBox('This is a test', 'l');
+            this.messageBox('To make sense of it, try to remove minimum height & height -if it’s there, or give it height: 0 - on the parent, then add some height to this absolute-positioned child and see what happens. My guess now you will see the parent’s gone (u cant see it because it has no height) & u only see the child, or u see nothing at all - that is, if the parent is assigned with property', 'l');
             this.messageBox('This is another test', 'r');
+            this.messageBox('This is another test', 'r')
+            this.messageBox('This is another test', 'r')
+            this.messageBox('This is another test', 'r')
+            this.messageBox('This is another test', 'r')
+            this.messageBox('To make sense of it, try to remove minimum height & height -if it’s there, or give it height: 0 - on the parent, then add some height to this absolute-positioned child and see what happens. My guess now you will see the parent’s gone (u cant see it because it has no height) & u only see the child, or u see nothing at all - that is, if the parent is assigned with property', 'l');
+
             
         } catch (error) {
             console.log('mount hook error:', error)
