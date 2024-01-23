e5107aeaf56666e380d031df1c915d367f621bdb
unknown <alch3mist94@protonmail.com>
2024-01-23 15:26:38 +0100

    recent state working on WIN

---
+++ examples/vicuna7B.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+How to get Vicuna-7B in GPTQ format to run.
+Please see the client arguments.
+'''
+
+from blowtorch import client, webUI
+
+cl = client(hugging_face_path='TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ', 
+            name='GPT',
+            device='cpu', 
+            device_map="auto",
+            trust_remote_code=True,
+            revision="main")
+
+cl.setConfig(
+    username='User',
+    do_sample=True, 
+    temperature=0.8, 
+    repetition_penalty=1.1
+)
+
+# cl.chat()
+webUI(cl)
