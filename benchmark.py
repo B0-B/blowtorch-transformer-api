86907cbea96716bd3837480bbc3d44133de09d79
unknown <alch3mist94@protonmail.com>
2024-03-08 17:29:28 +0100

    added new model benchmark

---
+++ benchmark.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+LLaMA2 benchmark example.
+'''
+
+from blowtorch import client
+
+cl = client(hugging_face_path='NousResearch/Llama-2-7b-chat-hf', 
+                name='GPT',
+                device='gpu', 
+                model_type="llama")
+
+cl.bench(token_length=512)
