76a334058b9bc70b76ca827857bd62870833d507
unknown <alch3mist94@protonmail.com>
2024-05-06 22:26:52 +0200

    added llama7-70B benchmark

---
+++ benchmark_llama3_70B_gpu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+Llama-3-70B GPU benchmark example.
+'''
+
+from blowtorch import client
+
+cl = client(hugging_face_path='NousResearch/Meta-Llama-3-70B', 
+            name='Assistant',
+            device='gpu',
+            chat_format="llama-3")
+
+cl.bench(tokens=512)
