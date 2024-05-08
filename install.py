730622f52c0542225599216c5edb175ca657efc0
unknown <alch3mist94@protonmail.com>
2024-05-08 13:47:50 +0200

    added new gpu test script

--- install.py
+++ install.py
@@ -4,7 +4,7 @@
 # https://github.com/B0-B/blowtorch-transformer-api
 
 
-import platform
+
 from os import system as shell, chdir
 from pathlib import Path
 
@@ -15,6 +15,23 @@ chdir(__root__)
 with open(__root__.joinpath('version')) as f:
     version = f.read()
 print(f'==== 🔥 blowtorch {version} 🔥 ====')
-# find wheel file and instal with pip manager
+
+# try to import torch
+try:
+    import torch
+except ImportError:
+    print('[error] blowtorch demands an installed torch build, to ensure that the specific backend is setup like drivers etc.\nPlease install torch (pip install torch) and run again.')
+    exit()
+
+# find wheel file and install with pip manager
 wheel = './dist/' + list(__root__.glob('./dist/*.whl'))[0].name
 shell(f'pip install {wheel}')
+
+# check for GPU specific backend
+if torch.cuda.is_available():
+    device_name = torch.cuda.get_device_name(0)
+    if 'amd' in device_name.lower():
+        print(f'[GPU] AMD device detected: {device_name}')
+    else:
+        # assume NVIDIA otherwise
+        print(f'[GPU] NVIDIA device detected: {device_name}')
